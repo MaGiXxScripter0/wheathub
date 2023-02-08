538bd58b31bb330657c8414c40c7f17dd944dcb2
MaGiXxScripter0 <123148797+MaGiXxScripter0@users.noreply.github.com>
2023-02-08 16:57:58 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,13 @@
+local url_raw_github = "https://raw.githubusercontent.com/MaGiXxScripter0/wheathub"
+
+local games_supported = {
+    [7499189111] = "/main/scripts/Encounters.lua",
+}
+
+function load_game()
+    local url_script = games_supported[game.PlaceId]
+    if not url_script then
+        return game.Players.LocalPlayer:Kick("\nNo Supported Game")
+    end
+    loadstring(game:HttpGet(url_raw_github + url_script))()
+end
