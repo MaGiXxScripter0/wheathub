47c05a8d9b081b0da3e648165887988073deb619
MaGiXxScripter0 <123148797+MaGiXxScripter0@users.noreply.github.com>
2023-02-09 09:41:46 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,7 @@ local url_raw_github = "https://raw.githubusercontent.com/MaGiXxScripter0/wheath
 
 local games_supported = {
     [7499189111] = "/main/scripts/Encounters.lua",
+    [7501699167] = "/main/scripts/Encounters.lua",
 }
 
 return function()
