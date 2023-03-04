f721ae8128489580c32969ca7a469332ae57e8ff
MaGiXxScripter0 <123148797+MaGiXxScripter0@users.noreply.github.com>
2023-03-04 14:22:01 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,7 +5,8 @@ local games_supported = {
     [7501699167] = "/main/scripts/Encounters.lua",
     [10446125875] = "/main/scripts/AnimeFruitSimulator.lua",
     [11542692507] = "/main/scripts/AnimeSoulsSimulator.lua",
-    [10036492542] = "/main/scripts/Get%20Muscles%20Simulator.lua"
+    [10036492542] = "/main/scripts/Get%20Muscles%20Simulator.lua",
+    [8540346411] = "/main/scripts/Rebirth%20Champions%20X.lua"
 }
 
 return function()
